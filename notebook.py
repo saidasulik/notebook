f4706a52deadb3d15030bec867d51318f9ce01e5
Sulik <saidasulik37@gmail.com>
2026-06-07 00:10:03 +0300

    menu

--- notebook.py
+++ notebook.py
@@ -1,2 +1,21 @@
 print("мой ноутбук")
-print("нет, не твой")
+print("==============")
+
+
+# Когда переменную вызывают, обычно, ей сразу присваивают какие то данные
+#cat_name = "Арчи"
+box = input("введите задачу: ")
+print(box)
+menu = True
+menu_number = 0
+# 1. напечатать снайперскую винтовку
+
+# цикл while (вайл)
+while  menu:
+    menu_number = input("выберите опцию: ")
+    menu_number = int(menu_number)
+    if(menu_number == 1):
+        print(box)
+    if(menu_number == 2):
+        menu = False
+        print('=====КОНЕЦ=====')
