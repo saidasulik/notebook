6447f3a742d592ec21b01ed767d7e5e3fef8a86f
Sulik <saidasulik37@gmail.com>
2026-06-28 22:10:21 +0300

    добавила список

--- notebook.py
+++ notebook.py
@@ -1,21 +1,26 @@
-print("мой ноутбук")
-print("==============")
-
 
 # Когда переменную вызывают, обычно, ей сразу присваивают какие то данные
 #cat_name = "Арчи"
-box = input("введите задачу: ")
-print(box)
+box = []
 menu = True
 menu_number = 0
 # 1. напечатать снайперскую винтовку
 
 # цикл while (вайл)
 while  menu:
+    print("===Меню===")
+    print("1.Показать список")
+    print("2.Выход из программы")
+    print("3.Добавить задачу")
+    print("==============") 
     menu_number = input("выберите опцию: ")
     menu_number = int(menu_number)
     if(menu_number == 1):
-        print(box)
+        for index,x in enumerate(box):
+            print(str(index + 1) + ": " + x)
     if(menu_number == 2):
         menu = False
         print('=====КОНЕЦ=====')
+    if(menu_number == 3):
+        spisok = input("введите задачу: ")
+        box.append(spisok)
