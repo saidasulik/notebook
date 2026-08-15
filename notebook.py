86882c6239a86cd3d42270c35baae15eebd8f580
Sulik <saidasulik37@gmail.com>
2026-08-16 00:23:44 +0300

    dead and sort

--- notebook.py
+++ notebook.py
@@ -1,30 +1,41 @@
+import csv
+def spisok1():
+    print("===Меню===")
+    print("1.Показать список")
+    print("2.Выход из программы")
+    print("3.Добавить новый продукт")
+    print("==============") 
 # Открываем файл с автоматическим закрытием после блока
-content = ""
-with open('list.txt', 'r', encoding="utf-8") as file:
-    content = file.read().splitlines()
-    
+data = []
+with open('list.csv', 'r', encoding="utf-8") as file:
+    content = csv.reader(file)
+    data = list(content)      
 # Файл уже закрыт
-
-
+print(data)
 # Когда переменную вызывают, обычно, ей сразу присваивают какие то данные
 #cat_name = "Арчи"
 box = []
 menu = True
 menu_number = 0
-# 1. напечатать снайперскую винтовку
 
 # цикл while (вайл)
-while  menu:
-    print("===Меню===")
-    print("1.Показать список")
-    print("2.Выход из программы")
-    print("3.Добавить задачу")
-    print("==============") 
+while  menu:  
+    spisok1()
     menu_number = input("выберите опцию: ")
     menu_number = int(menu_number)
-    if(menu_number == 1):
-        for index,element in enumerate(content):
-          print(str(index + 1) + ": " + element)
+    if(menu_number == 1):    
+        for index,element in enumerate(data):
+          pravda = False
+          galochca = ""
+          if(element[1] == "True"): 
+              pravda = True
+          if pravda:
+              galochca = "☑️"     
+          if not pravda:
+              galochca = "🔲"             
+          print(galochca + " " + element[0])
+          
+          
         
     if(menu_number == 2):
         menu = False
